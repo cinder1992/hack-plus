5b3fa6f6f9b9e659281618a6e552d7d4f26afd90
cinder1992 <cinder5832@gmail.com>
2013-12-17 15:17:07 +0000

    Added drawing

--- main.pl
+++ main.pl
@@ -5,12 +5,13 @@ use SDL;
 use SDLx::App;
 use SDLx::Surface;
 use SDL::Image;
+use SDL::GFX::Rotozoom;
 use SDLx::Rect;
 use SDL::Event;
 use SDLx::Text;
-use constant SCREEN_W => 1024;
-use constant SCREEN_H => 768;
-my ($exiting, $time, $srcRect, $dstRect, $sprite, @particles, $event, @mouse, $colRect, $spawn);
+use constant SCREEN_W => 800;
+use constant SCREEN_H => 600;
+my ($exiting, $srcRect, $dstRect, $sprite, $event, @mouse, $backdrop, $roomArea, @room);
 
 my $app = SDLx::App->new(   #Create Window
   w => SCREEN_W,
@@ -20,18 +21,60 @@ my $app = SDLx::App->new(   #Create Window
   exit_on_quit => 1
 );
 
-$sprite = SDL::Image::load( 'img/guy.png' ) or die("Could not load image!"); #load the particle image
-$srcRect = SDLx::Rect->new(0,0,10,20);
-$dstRect = SDLx::Rect->new(SCREEN_W / 2, SCREEN_H / 2, 10, 20);
-$event=SDL::Event->new();    # create one global event
+$roomArea = <<EOR
+........
+...##...
+..#..#..
+.#....#.
+.#....#.
+..#..#..
+...##...
+........
+EOR
+;
+@room = ();
+my $wall = SDL::Image::load( 'img/room/wall.png' ) or die("Could not load wall image!"); #Load the wall image
+my $tile = SDL::Image::load( 'img/room/tile.png' ) or die("Could not load tile image!");
+$backdrop = SDLx::Surface->new( width => 800, height => 600 );
+my $src = SDLx::Rect->new(0,0,800,600); #set up source and dest
+my $dst = SDLx::Rect->new(0,0,800,600);
+my $virtX = 0; #create our virtual coordinates
+my $virtY = 0;
 
-while(!$exiting) {
-  $app->draw_rect([0, 0, SCREEN_W, SCREEN_H], [255, 255, 255, 255]);
-  $app->blit_by($sprite, $srcRect, $dstRect);
-  $app->update();
-  handleEvents();
+foreach my $line (split("\n", $roomArea)) {
+  foreach my $char (split("", $line)) {
+    push @{$room[$virtX]}, $char;
+    $virtX++;
+  }
+  $virtX = 0; #Reset virtual X
 }
 
+##Compute best-fit##
+my $offset = ((800/2) - 14) - ($#{$room[0]}*14);
+
+for my $x (0 .. $#room) {
+  for my $y (0 .. $#{$room[$x]}) {
+    my $char = $room[$x][$y];
+    $dst->x(((800/2) - (14)) + (($x*14) - ($y*14)) - $offset); #compute virtual -> real coords
+    $dst->y(($x+$y)*7);    
+    if ($char eq '.') {
+      $app->blit_by($tile, $src, $dst);
+    }
+    elsif ($char eq '#') {
+      $dst->y($dst->y - 14);
+      $app->blit_by($wall, $src, $dst);
+    }
+  }
+}
+$app->update();
+
+$srcRect = SDLx::Rect->new(0,0,800,600);
+$dstRect = SDLx::Rect->new(0,0,400,800);
+$event = SDL::Event->new();    # create one global event
+
+while(!$exiting) {
+  handleEvents();
+}
 
 sub mouseEvent {
   my($mouse_mask, $mouse_x, $mouse_y) = @{SDL::Events::get_mouse_state()};
@@ -50,7 +93,6 @@ sub handleEvents {
     elsif($event->type == SDL_MOUSEBUTTONDOWN)
     {
       &mouseEvent($event);
-      $spawn = 1
     }
   }
 }
