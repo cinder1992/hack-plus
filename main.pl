c4933558f6421109722cd1cadaad091397b3593b
cinder1992 <cinder5832@gmail.com>
2014-01-06 23:00:34 +0000

    Converted graphics calls to OpenGL from SDL

--- main.pl
+++ main.pl
@@ -2,14 +2,10 @@
 use strict;
 use warnings;
 use SDL;
-use SDL::Video;
 use SDLx::App;
-use SDLx::Surface;
-use SDLx::Sprite;
-use SDL::Image;
-use SDL::GFX::Rotozoom;
 use SDLx::Rect;
 use SDL::Event;
+use OpenGL qw(:all);
 use constant SCREEN_W => 800;
 use constant SCREEN_H => 600;
 my ($exiting, $srcRect, $dstRect, $sprite, $event, @mouse, $roomArea, @room);
@@ -17,10 +13,17 @@ my ($exiting, $srcRect, $dstRect, $sprite, $event, @mouse, $roomArea, @room);
 my $app = SDLx::App->new(   #Create Window
   w => SCREEN_W,
   h => SCREEN_H,
-  d =>24,
   title => "I'm a particle!",
-  exit_on_quit => 1
+  exit_on_quit => 1,
+  gl => 1
 );
+#define openGL functions
+glEnable(GL_DEPTH_TEST);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity;
+gluPerspective(60, SCREEN_W / SCREEN_H, 1, 1000);
+glTranslatef(0, 0, -20);
+glutInit();
 
 $roomArea = <<EOR
 ..####..
@@ -35,11 +38,6 @@ EOR
 ;
 @room = ();
 
-my $wall = SDLx::Sprite->new( image => "img/room/wall.png" ) or die("Could not load wall image!"); #Load the wall image
-my $tile = SDLx::Sprite->new( image => "img/room/tile.png" ) or die("Could not load tile image!");
-#my $backdrop = SDLx::Surface->new( width => 800, height => 600, depth => 32);
-#SDL::Video::set_alpha( $backdrop, 0, 100);
-#$backdrop->draw_rect([0,0,800,600],[0,0,0,255]);  #
 my $virtX = 0;
 
 foreach my $line (split("\n", $roomArea)) {
@@ -50,33 +48,30 @@ foreach my $line (split("\n", $roomArea)) {
   $virtX = 0; #Reset virtual X
 }
 
-##Compute best-fit##
-my $offset = ((800/2) - 14) - ($#{$room[0]}*14);
-
-for my $x (0 .. $#room) {
-  for my $y (0 .. $#{$room[$x]}) {
-    my $char = $room[$x][$y];
-    my $dstx = ((800/2) - (14)) + (($x*14) - ($y*14)) - $offset; #compute virtual -> real coords
-    my $dsty = ($x+$y)*7;    
-    if ($char eq '.') {
-      $tile->x($dstx);
-      $tile->y($dsty);
-      $tile->draw($app);
-    }
-    elsif ($char eq '#') {
-      $wall->x($dstx);
-      $wall->y($dsty - 14);
-      $wall->draw($app);
-    }
-  }
-}
-$srcRect = SDLx::Rect->new(0,0,800,600);
-$dstRect = SDLx::Rect->new(0,0,30,15);
 $event = SDL::Event->new();    # create one global event
 
-#my $zoomWin = SDL::GFX::Rotozoom::surface( $backdrop, 0, 1, SMOOTHING_OFF);
-while(!$exiting) {
-  $app->flip();
+while (!$exiting) {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glRotatef(0.1, 1, 0, 1);
+  for my $x (0 .. $#room) {
+    for my $y (0 .. $#{$room[$x]}) {
+      my $char = $room[$x][$y];
+      glMatrixMode(GL_MODELVIEW);
+      glPushMatrix();
+      glTranslatef($x, 0, $y);
+      if ($char eq '.') {
+        glColor3d(0,0.5,0);
+        glutSolidCube(1);
+      }
+      elsif ($char eq '#') {;
+        glTranslatef(0, 1, 0);
+        glColor3d(0.2,0.2,0.2);
+        glutSolidCube(1);
+      }
+      glPopMatrix();
+    }
+  } 
+  $app->sync();
   handleEvents();
 }
 
@@ -100,4 +95,3 @@ sub handleEvents {
     }
   }
 }
-
