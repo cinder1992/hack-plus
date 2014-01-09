3cf0a03c22369a1beb02351b06f89d31168987d7
cinder1992 <cinder5832@gmail.com>
2014-01-09 22:50:45 +0000

    Vastly improved world engine and lighting engine

--- main.pl
+++ main.pl
@@ -43,14 +43,14 @@ $camera{'x'} = 0; $camera{'y'} = 15; $camera{'z'} = -15;
 glutInit();
 
 $roomArea = <<EOR
-..##.#..
-.#....#.
-#......#
-#l.....#
-#......#
-#......#
-.#....#.
-..####..
+  ....  
+ ...... 
+........
+..l.....
+........
+........
+ ...... 
+  .... 
 EOR
 ;
 @room = ();
@@ -64,6 +64,7 @@ foreach my $line (split("\n", $roomArea)) {
   }
   $virtX = 0; #Reset virtual X
 }
+
 $deltaYaw = 0;
 $deltaPitch = 0;
 $event = SDL::Event->new();    # create one global event
@@ -71,37 +72,10 @@ gluLookAt($camera{'x'}, $camera{'y'}, $camera{'z'}, 0, 0, 0, 0, 1, 0);
 while (!$exiting) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glRotatef($deltaYaw, 0, 1, 0);
-  #glRotatef($deltaPitch, 1, 0, 0);
+  glRotatef($deltaPitch, 1, 0, 0);
   $deltaYaw = 0;
   $deltaPitch = 0;
-  for my $x (0 .. $#room) {
-    for my $y (0 .. $#{$room[$x]}) {
-      my $char = $room[$x][$y];
-      glMatrixMode(GL_MODELVIEW);
-      glPushMatrix();
-      glTranslatef($x - 3.5, 0, $y - 3.5);
-      if ($char eq '.') {
-        glColor3d(0,0.5,0);
-        draw_cube(1, GL_TRIANGLES);
-      }
-      elsif ($char eq '#') {;
-        glColor3d(0,0.5,0);
-        draw_cube(1, GL_TRIANGLES);
-        glTranslatef(0, 1, 0);
-        glColor3d(0.2,0.2,0.2);
-        draw_cube(1, GL_TRIANGLES);
-        glTranslatef(0, 1, 0);
-        draw_cube(1, GL_TRIANGLES);
-      }
-      elsif ($char eq 'l') {
-        my @lightPos = ($x - 3.5, 1.5, $y - 3.5);
-        glLightfv_p(GL_LIGHT0, GL_POSITION, @lightPos, 1);
-        glColor3d(0,0.5,0);
-        draw_cube(1, GL_TRIANGLES);
-      }
-      glPopMatrix();
-    }
-  } 
+  positionLight(drawWorld(@room));
   $app->sync();
   handleEvents();
 }
@@ -109,6 +83,7 @@ while (!$exiting) {
 sub quitEvent {
   exit;
 }
+
 sub handleEvents {
   SDL::Events::pump_events();
   my $key = ($event->type == 2 or $event->type == 3) ? $event->key_sym : "";
@@ -126,8 +101,7 @@ sub handleEvents {
   }
 }
 
-sub draw_cube
-{
+sub draw_cube {
   my $mul = shift;
   my $type = shift;
   # A simple cube
@@ -151,4 +125,127 @@ sub draw_cube
     }
   }
   glEnd;
+}
+
+sub drawWorld {
+  my @world = @_;
+  glPushMatrix(); #Reset Matrix
+  my $maxY = 0;
+  glBegin(GL_TRIANGLES);
+  my @offset = (0, 0,0);
+  my @lightPos;
+  for my $x (0 .. $#world) {
+    glTranslatef(1,0,0); #translate according to our coordinate
+    for my $y (0 .. $#{$world[$x]}) {
+      my @grid;
+      my $char = $world[$x][$y];
+      my $i;
+      for ($i = 0, $i <= 8, $i++) {
+        if($i < 3) {
+          if ($x > 0 && $y > 0) {push(@grid, $world[$x-1][$y-1+$i] eq '#' ? 2 : (($world[$x-1][$y-1+$i] eq " " or "") ? 0 : 1))}
+          else {push(@grid,0);}
+        }
+        if($i > 3) {
+          if ($x < 7 && $y < 7) { push(@grid, $world[$x+1][$y-1+($i-6)] eq '#' ? 2 : (($world[$x+1][$y-1+($i-6)] eq " " or "") ? 0 : 1))}
+          else {push(@grid,0)}
+        }
+        else {
+          if($y < 0 || $y > 7) { push(@grid, 0) }
+          else { push(@grid, $world[$x][$y-1+($i-3)] eq '#' ? 2 : (($world[$x][$y-1+($i-6)] eq " " or "") ? 0 : 1 ))}
+        }
+      }
+
+      my @localOffset = ($offset[0] + $x, $offset[1], $offset[2] + $y);
+      if ($char eq '.') {
+        glColor3d(0,0.5,0);
+        drawFloor($localOffset[0], $localOffset[1], $localOffset[2], floorNormals(@grid));
+      }
+      elsif ($char eq '#') {;
+        glColor3d(0,0.5,0);
+        drawFloor($localOffset[0], $localOffset[1], $localOffset[2], floorNormals(@grid));
+        glColor3d(0.2,0.2,0.2);
+      }
+      elsif ($char eq 'l') {
+        @lightPos = ($x, 1.5, $y);
+        glColor3d(0,0.5,0);
+        drawFloor($localOffset[0], $localOffset[1], $localOffset[2], floorNormals(@grid));
+      }
+      $maxY=$y;
+    }
+    glTranslatef(0,0,-$maxY-1);
+  } 
+  glEnd();
+  glPopMatrix();
+  return @lightPos;
+}
+
+sub positionLight {
+  my @light = @_;
+  glLightfv_p(GL_LIGHT0, GL_POSITION, @light, 0);
+  glLightfv_p(GL_LIGHT0, GL_POSITION, $light[0]+2,$light[1]+2,$light[2]+2, 1);
+}
+
+sub drawFloor {
+  my $xOff = shift;
+  my $yOff = shift;
+  my $zOff = shift;
+  my $normals = shift;
+  # A cubic world node
+  my @indices = qw( 6 7 3  3 2 6
+                    5 6 2  2 1 5
+                    4 5 1  1 0 4
+                    7 4 0  0 3 7
+                    5 4 7  7 6 5
+                    0 1 2  2 3 0);
+
+  my @vertices = ([-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [0.5, -0.5, 0.5], [-0.5, -0.5, 0.5],
+                  [-0.5,  0.5, -0.5], [0.5,  0.5, -0.5], [0.5,  0.5, 0.5], [-0.5,  0.5, 0.5]);
+
+  foreach my $triangle (0 .. 11) {
+    foreach my $vertex (0 .. 2) {
+      my $index  = $indices[3 * $triangle + $vertex];
+      my $coords = $vertices[$index];
+      my $normal = $$normals[$index];
+      glNormal3f($$normal[0] + $xOff, $$normal[1] + $yOff, $$normal[2] + $zOff);
+      glVertex3f($$coords[0] + $xOff, $$coords[1] + $yOff, $$coords[2] + $zOff);
+    }
+  }
+}
+
+sub floorNormals {
+  my @grid = @_; #get the 3x3 grid to check to generate normals
+  my @normals = ([-1, -1, -1], [1, -1, -1], [1, -1, 1], [-1, -1, 1],
+                 [-1,  1, -1], [1,  1, -1], [1,  1, 1], [-1,  1, 1]);
+
+  my @changeValues = ([0,3,1], [2,1,5], [8,5,7], [6,7,3]); #what grid unit can change which vertix (top or bottom)
+  my $i;
+  for ($i = 0, $i <= 3, $i++) { #calculate the bottom face first
+    my $ch = $changeValues[$i]; #get the correct values for this vertex
+    if ($grid[$$ch[0]] && $grid[$$ch[1]] && $grid[$$ch[2]]) { #are all three slots filled?
+      $normals[$i] = [0, -1, 0]; #if so, set the normal to DOWN
+    }
+    elsif ($grid[$$ch[0]] && !$grid[$$ch[1]] && $grid[$$ch[2]]) {
+      $normals[$i] = [(-1*cos(-90*$i))-(1*sin(-90*$i)), -1, (-1*sin(-90*$i))+(1*cos(-90*$i))]; #make our normal vector and transform to the correct angle
+    }
+    elsif ($grid[$$ch[0]] && $grid[$$ch[1]] && !$grid[$$ch[2]]) {
+      $normals[$i] = [(1*cos(-90*$i))-(-1*sin(-90*$i)), -1, (1*sin(-90*$i))+(-1*cos(-90*$i))];  #this is an abomination of code
+    }
+  }
+
+  for ($i = 4, $i <= 7, $i++) {
+    my $ch = $changeValues[$i - 4]; #calculate based on the new $i value
+    if (($grid[$$ch[0]] == 1) && ($grid[$$ch[1]] == 1) && ($grid[$$ch[2]] == 1)) { #are all three slots filled with floors?
+      $normals[$i] = [0, 1, 0]; #if so, set the normal to UP
+    }
+    if (($grid[$$ch[0]] == 2) && ($grid[$$ch[1]] == 2) && ($grid[$$ch[2]] == 2)) { #are all three slots filled with walls?
+      $normals[$i] = [(1*cos(-90*$i))-(1*sin(-90*$i)), 1, (1*sin(-90*$i))+(1*cos(-90*$i))]; #reverse the normal if so
+    }
+    elsif ($grid[$$ch[0]] && !$grid[$$ch[1]] && $grid[$$ch[2]]) {
+      $normals[$i] = [(-1*cos(-90*$i))-(1*sin(-90*$i)), 1, (-1*sin(-90*$i))+(1*cos(-90*$i))]; #make our normal vector and transform to the correct angle
+    }
+    elsif ($grid[$$ch[0]] && $grid[$$ch[1]] && !$grid[$$ch[2]]) {
+      $normals[$i] = [(1*cos(-90*$i))-(-1*sin(-90*$i)), 1, (1*sin(-90*$i))+(-1*cos(-90*$i))];  #this is an abomination of code
+    }
+  }
+  return \@normals;
 }
