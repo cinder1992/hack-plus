93c23cfb751844b499c5803ca86a339e02c6430d
Corey O'Regan <it.coreyor@localhost.localdomain>
2014-01-31 11:23:04 +0000

    added collision detection code

---
+++ Entity/data.pm
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+package Entity::data;
+use strict;
+use warnings;
+use SDL;
+use SDL::Event;
+use SDL::Events;
+use SDLx::App;
+use SDL::GFX::Rotozoom;
+use SDLx::Sprite;
+
+# enable export of sundry bits
+require Exporter;
+our @ISA = qw(Exporter);
+#
+# commence data structures
+our ($roomArea, @room);
+# prep for export by listing
+our @EXPORT_OK= qw ($roomArea @room);
+# now export
+our %EXPORT_TAGS = (all => [@EXPORT_OK]);
+# return 'true'
+return 1;
